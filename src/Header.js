064e13766dca2cdec96addac6d2c53171333c462
jbalena09 <45332736+jbalena09@users.noreply.github.com>
2021-07-01 17:14:51 +0800

    Updated responsive site
    Updated responsive site

--- src/Header.js
+++ src/Header.js
@@ -1,30 +1,61 @@
+import React, { useState } from "react";
 import "./index.css";
 import image from "./logo.svg";
+import nav from "./images/nav.png";
 
 function Header() {
+  const [navbar, setNav] = useState(false);
+  const [dropDown, setDropDown] = useState(true);
+
+  const backgroundChanger = () => {
+    if (window.scrollY >= 30) {
+      setNav(true);
+    } else {
+      setNav(false);
+    }
+  };
+  window.addEventListener("scroll", backgroundChanger);
+
+  let flag = true;
+  const navClick = () => {
+    if (dropDown) {
+      setDropDown(false);
+    } else {
+      setDropDown(true);
+      flag = true;
+    }
+  };
+
   return (
-    <div className="Header">
-      <a href="#">
-        <img src={image} width="154px" height="24" />
-      </a>
-      <ul>
-        <li>
-          <a href="#" className="list">
-            Movies
-          </a>
-        </li>
-        <li>
-          <a href="#" className="list">
-            Tv Show
-          </a>
-        </li>
-        <li>
-          {" "}
-          <a href="#" className="list">
-            More
-          </a>
-        </li>
-      </ul>
+    <div className={navbar ? "Header fixed" : "Header"}>
+      <div className="non-drop-down">
+        <a href="#">
+          <img src={image} width="154px" height="24" />
+        </a>
+        <span>
+          <img src={nav} width="20px" onClick={() => navClick()} />
+        </span>
+      </div>
+      <div className={dropDown ? "drop-down-hidden" : "drop-down"}>
+        <ul>
+          <li>
+            <a href="#" className="list">
+              Movies
+            </a>
+          </li>
+          <li>
+            <a href="#" className="list">
+              Tv Show
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a href="#" className="list">
+              More
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
